11c0be21a1aec705b75e1807eb28d617f6400b94
Justin <Justin@JUSTIN-VAIO>
2014-04-03 15:37:19 +0200

    Ajout de fonction pour gérer la base de données.

--- MediaStoreProject/src/main/java/main/Bdd.java
+++ MediaStoreProject/src/main/java/main/Bdd.java
@@ -18,24 +18,70 @@ import org.h2.tools.DeleteDbFiles;
  */
 public class Bdd {
 
-    
+    Connection conn;
     public static void main(String args[]) throws ClassNotFoundException, SQLException{
+       addImage("test");
+       getAllImage();
+    }
+   
+    public static void addImage(String path) throws ClassNotFoundException, SQLException{
         DeleteDbFiles.execute("~", "test", true);
         Class.forName("org.h2.Driver");
         Connection conn = DriverManager.getConnection("jdbc:h2:./src/main/resources/mediaproject","root","root");
         java.sql.Statement stat = conn.createStatement();
-        //stat.execute("create table test(id int primary key, name varchar(255))");
-        //stat.execute("insert into FILES values(1,'Hello')");
-        ResultSet rs;
-        rs = stat.executeQuery("select * from FILES");
-        while (rs.next()) {
-            System.out.println(rs.getString("ID_FILE"));
-            
-        }
-        System.out.println("ok bdd");
+        stat.execute("insert into FILES values(1,'Hello',1,1,'n','n',1,1)");
+        System.out.println("Ajout de l'image");
         stat.close();
         conn.close();
     }
     
-
+    public static void getAllImage() throws ClassNotFoundException, SQLException{
+        DeleteDbFiles.execute("~", "test", true);
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:./src/main/resources/mediaproject","root","root");
+        java.sql.Statement stat = conn.createStatement();
+        ResultSet rs;
+        rs = stat.executeQuery("select * from FILES");
+        while (rs.next()) {
+            System.out.println("Image numéro :");
+            System.out.println(rs.getString("ID_FILE"));
+            
+        }
+        stat.close();
+        conn.close();
+    }
+     public static void getImage(int id) throws ClassNotFoundException, SQLException{
+        DeleteDbFiles.execute("~", "test", true);
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:./src/main/resources/mediaproject","root","root");
+        java.sql.Statement stat = conn.createStatement();
+        String requeteSQL = "SELECT * FROM FILES WHERE ID_File = '";
+        String concat = requeteSQL.concat(id + "'");
+        ResultSet rs;
+        rs = stat.executeQuery(concat);
+        while (rs.next()) {
+            System.out.println("Image numéro :");
+            System.out.println(rs.getString("ID_FILE"));
+            
+        }
+        stat.close();
+        conn.close();
+    } 
+    public static void delImage(int id) throws ClassNotFoundException, SQLException{
+        DeleteDbFiles.execute("~", "test", true);
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:./src/main/resources/mediaproject","root","root");
+        java.sql.Statement stat = conn.createStatement();
+        String requeteSQL = "DELETE FROM FILES WHERE ID_File = '";
+        String concat = requeteSQL.concat(id + "'");
+        ResultSet rs;
+        rs = stat.executeQuery(concat);
+        while (rs.next()) {
+            System.out.println("Image numéro :");
+            System.out.println(rs.getString("ID_FILE"));
+            
+        }
+        stat.close();
+        conn.close();
+    } 
 }
